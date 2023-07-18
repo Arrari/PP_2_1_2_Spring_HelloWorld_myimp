82c26349efa0a8cbd955605bd1fa5da567dfdfeb
a_khoruzhenko <khoruzhenko-ad@dep124.avt>
2023-07-18 15:07:51 +0500

    Small test, working as intended

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,9 +7,16 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld hwbean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println(bean == hwbean2);
         Cat bean2 = (Cat) applicationContext.getBean("catBean", "kitty1","orange");
         System.out.println(bean2);
+        Cat bean3 = (Cat) applicationContext.getBean("catBean", "kitty2","black and white");
+        System.out.println(bean3);
+        System.out.println(bean2==bean3);
+
 
     }
 }
